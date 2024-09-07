47080920ce5a82a2586590ba7864af3c525d67ea
CodeWithKrish12 <codewithkrish12@gmail.com>
2024-09-07 11:58:34 +0530

    feat: Initialised shadcn and next theme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked"
   ],
   "rules": {
+    "@typescript-eslint/no-require-imports": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
