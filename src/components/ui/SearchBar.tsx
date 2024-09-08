a09c215992a7cb5d6226d15ed2833823955acf92
aryansharma2k4 <aryansharma2k4@gmail.com>
2024-09-08 17:14:45 +0530

    Search Bar updated

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -4,7 +4,7 @@ import { PlaceholdersAndVanishInput } from "../ui/placeholders-and-vanish-input"
 
 export function SearchBar() {
   interface Suggestion {
-    _id: string;
+    _id: string;git 
     name: string;
     location: string;
     description: string;
